ddca36cb7174df10c1f5c72b684338e951854afe
Artem <2929348+leipreachan@users.noreply.github.com>
2026-01-13 23:49:13 +0000

    improved hitcard

--- src/hitcard.tsx
+++ src/hitcard.tsx
@@ -1,17 +1,18 @@
 import { Brevet } from './types';
 
 export function HitCard({ hit }: { hit: Brevet }) {
+  const maps = hit.map?.filter((item) => item);
   return (
     <div
       data-objectid={hit.objectID}
-      style={{ position: 'relative', width: '100%' }}
+      style={{ position: 'relative', width: '100%', verticalAlign: 'top' }}
     >
       <a href={`?objectID=${hit.objectID}`} style={{ float: 'right' }}>
         share
       </a>
       <h2>{hit.date}</h2>
       <p>{hit.distance} km</p>
-      {Boolean(hit.name) && <p>{hit.name}</p>}
+      {Boolean(hit.name) && <h3>{hit.name}</h3>}
       <p>
         {[hit.city, hit.department, hit.region, hit.country]
           .filter(Boolean)
@@ -20,16 +21,16 @@ export function HitCard({ hit }: { hit: Brevet }) {
       {Boolean(hit.ascent) && <p>{hit.ascent} m</p>}
       {Boolean(hit.site) && (
         <a
-          href={hit.site && hit.site.startsWith('http') ? hit.site : undefined}
+          href={hit.site}
           target="_blank"
         >
           {hit.site}
         </a>
       )}
-      <p>{hit.mail}</p>
-      <p>{hit.club}</p>
+      <p><a href={'mailto:' + hit.mail}>{hit.mail}</a></p>
+      <p>{hit.club} {hit?.meta?.Contact ? '(' + hit?.meta?.Contact + ')' : ''}</p>
       <ul>
-        {hit.map?.map((map: string) => (
+        {maps?.map((map: string) => (
           <li key={map}>
             <a href={map.startsWith('http') ? map : undefined} target="_blank">
               {map}
